d61f11c3c8aca1835f018682bc0f373b6ee5fe2a
Devin Hunter <devhunter16@gmail.com>
2024-11-11 11:01:05 -0700

    Made a reusable dashboard-item component and moved some html there.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { DashboardItemComponent } from './dashboard-items/dashboard-item/dashboard-item.component';
 import { ServerStatusComponent } from './dashboard-items/server-status/server-status.component';
 import { TrafficComponent } from './dashboard-items/traffic/traffic.component';
 import { SupportTicketsComponent } from './dashboard-items/support-tickets/support-tickets.component';
@@ -8,6 +9,7 @@ import { SupportTicketsComponent } from './dashboard-items/support-tickets/suppo
   selector: 'app-dashboard',
   standalone: true,
   imports: [
+    DashboardItemComponent,
     ServerStatusComponent,
     TrafficComponent,
     SupportTicketsComponent
