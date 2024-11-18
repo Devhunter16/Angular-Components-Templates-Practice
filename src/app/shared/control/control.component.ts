4b7e540e8113b4532766b00ab6e5d40d2dabab4a
Devin Hunter <devhunter16@gmail.com>
2024-11-18 09:11:33 -0700

    Using Angular's ViewEncapsulation feature to re-scope some styles in this component to be global styles.

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,16 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  // Adding the encapsulation setting so that we can use angular's 'ViewEncapsulation'
+  // feature which we call .none on which disables css style scoping (encapsulation) because we have
+  // several CSS styles in our .css file that actually need to be global (the "Title"
+  // and "Request" inputs)
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
   label = input.required<string>();
