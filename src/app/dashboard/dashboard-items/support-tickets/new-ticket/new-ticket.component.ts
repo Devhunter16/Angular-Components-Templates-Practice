a580a033b2766bde9046f3df826569b24474ea01
Devin Hunter <devhunter16@gmail.com>
2024-11-21 09:29:11 -0700

    Practicing with using some template variables in this form component.

--- src/app/dashboard/dashboard-items/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/dashboard-items/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../../shared/button/button.component';
 import { ControlComponent } from "../../../../shared/control/control.component";
@@ -11,5 +12,10 @@ import { ControlComponent } from "../../../../shared/control/control.component";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
-}
+  // Check out the template! We're passing our input elements to our function as
+  // arguments there!
+  onSubmit(ticketTitle: string, ticketText: string) {
+    const enteredTitle = ticketTitle;
+    console.log(enteredTitle);
+  };
+};
